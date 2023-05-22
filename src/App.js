d6c8706bc6ce2bea353af27c6900ed5e533ff426
ojingwaDaniel <ojingwadanny@gmail.com>
2023-05-22 22:48:33 +0100

    using useRef

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import "./App.css";
 import { useRef} from "react";
 
 function App() {
+  const colorName = useRef();
+  const hexColor = useRef();
+  const submit = e => {
+    e.preventDefault()
+    let name = colorName.current.value
+    let  color = hexColor.current.value
+    alert(`${name} , ${color}`)
+    colorName.current.value = ''
+    hexColor.current.value = ''
+  }
+  
+  
   return (
     <div className="App">
       <h1>A color Picker</h1>
-      <form action="">
-        <input type="text" placeholder="name of color" />
-        <input type="color" />
+      <form onSubmit={submit}>
+        <input type="text" placeholder="name of color"  ref={colorName}/>
+        <input type="color"  ref={hexColor}/>
         <button type="submit">Add</button>
       </form>
     </div>
