1f11d4652eecab65dc8e5c0acd17ce573301011e
ojingwaDaniel <ojingwadanny@gmail.com>
2023-05-22 22:03:15 +0100

    using userReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { useState } from "react";
+import { useReducer} from "react";
 
 function App() {
-  const [currentStatus,newStatus] = useState(false)
+  const [currentStatus,newStatus] = useReducer(newStatus(currentStatus => !currentStatus),(false))
  
   return (
     <div className="App">
       <input
         type="checkbox"
         value={currentStatus}
-        onChange={()=>newStatus(currentStatus => !currentStatus)}
+        onChange={newStatus}
         
       />
       <label>{ currentStatus ? 'Agreed to terms and Condtition' : 'Click to agree to terms and Conditions'}
