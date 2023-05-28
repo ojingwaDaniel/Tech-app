bc88ddef78c0906ff837c7144d2318a7d3e489a6
ojingwaDaniel <ojingwadanny@gmail.com>
2023-05-28 21:58:11 +0100

    React Hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
 import "./App.css";
-import { useState} from "react";
+import { useState,useEffect} from "react";
+// function useInput(initialValue){
+//   const [value , setValue] = useState(initialValue)
+//   return(
+//     [
+//     {value,onChange: e => setValue(e.target.value)},
+//     ()=> setValue(initialValue)
+//     ]
+//   )
+// }
 
-function App() {
-  const [name,setName] = useState("")
-  const [color,hexColor] = useState("#00000")
-  const submit = e => {
-    e.preventDefault()
-    alert(`${name} , ${color}`);
-    e.target.reset()
-  }
+// function App() {
+//   const [colorTitle,resetTitle] = useInput("")
+//   const [hexColor,resetColor] = useInput("#00000")
+//   const submit = e => {
+//     e.preventDefault()
+//     alert(`${colorTitle.value} , ${hexColor.value}`);
+//     resetTitle()
+//     resetColor()
+//   }
+//   return (
+//     <div className="App">
+//       <h1>A color Picker</h1>
+//       <form onSubmit={submit}>
+//         <input
+//           type="text"
+//           placeholder="name of color"
+//           {...colorTitle}
+//         />
+//         <input type="color" {...hexColor} />
+//         <button type="submit">Add</button>
+//       </form>
+//     </div>
+//   );
   
-  
-  return (
-    <div className="App">
-      <h1>A color Picker</h1>
-      <form onSubmit={submit}>
-        <input
-          type="text"
-          placeholder="name of color"
-          onChange={(event) => setName(event.target.value)}
-        />
-        <input type="color" onChange={(event)=> hexColor(event.target.value)} />
-        <button type="submit">Add</button>
-      </form>
+function App(){
+  const [data,setData] = useState(null)
+  useEffect(()=>{
+    fetch(`https://api.github.com/users/ojingwaDaniel`)
+    .then(response=> response.json())
+    .then(setData)
+  },[])
+  if (data)
+  return(
+   <pre>{JSON.stringify(data,null,2)}</pre>
+   )
+  return(
+    <div className= 'App'>
+      <h1>Data</h1>
     </div>
-  );
-  
-    
+  )
 }
-
 export default App;
