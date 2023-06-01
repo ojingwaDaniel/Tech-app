69b4638c407df3964db557671a5044d7adae9681
ojingwaDaniel <ojingwadanny@gmail.com>
2023-06-01 16:49:34 +0100

    added a react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState,useEffect} from "react";
+// import { useState,useEffect} from "react";
 
 // function useInput(initialValue){
 //   const [value , setValue] = useState(initialValue)
@@ -34,51 +34,82 @@ import { useState,useEffect} from "react";
 //     </div>
 //   );
 // }
-const query = `query{
-  allLifts{
-    name
-    elevationGain
-    status
-  }
-}`;
-const opt = {
-  method : 'POST',
-  headers : {'Content-Type' : 'application/json'},
-  body : JSON.stringify({query})
-}
-function Lift ({name,elevationGain,status}){
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{elevationGain} {status}</p>
-    </div>
-  );
+// const query = `query{
+//   allLifts{
+//     name
+//     elevationGain
+//     status
+//   }
+// }`;
+// const opt = {
+//   method : 'POST',
+//   headers : {'Content-Type' : 'application/json'},
+//   body : JSON.stringify({query})
+// }
+// function Lift ({name,elevationGain,status}){
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <p>{elevationGain} {status}</p>
+//     </div>
+//   );
+// }
+// function App(){
+//   const [data,setData] = useState(null)
+//   const [error,setError] = useState(null)
+//   const [loading,setLoading] = useState(false);
+//   useEffect(()=>{
+//     setLoading(true)
+//     fetch(`https://snowtooth.moonhighway.com/`,opt)
+//       .then((response) => response.json())
+//       .then(setData)
+//       .then(() => setLoading(false))
+//       .catch(setError);
+//   },[])
+//   if(loading) return <h1>Loading........</h1>
+//   if(error) <pre>{JSON.stringify(error)}</pre>
+//   if(!data) return null
+//   return (
+//    <div>
+//     {data.data.allLifts.map((lift)=> 
+//     <Lift name = {lift.name} 
+//     elevationGain ={lift.elevationGain}
+//      status = {lift.status}
+//      />)}
+//    </div>
+//   );
+// }
+const studentData = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 }
+];
+function StudentList({data,renderItem,renderEmpty}){
+  return !data.length ? renderEmpty :(
+    <ul>
+      {data.map(item=>(
+        <li key = {item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  )
 }
 function App(){
-  const [data,setData] = useState(null)
-  const [error,setError] = useState(null)
-  const [loading,setLoading] = useState(false);
-  useEffect(()=>{
-    setLoading(true)
-    fetch(`https://snowtooth.moonhighway.com/`,opt)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  },[])
-  if(loading) return <h1>Loading........</h1>
-  if(error) <pre>{JSON.stringify(error)}</pre>
-  if(!data) return null
-  return (
-   <div>
-    {data.data.allLifts.map((lift)=> 
-    <Lift name = {lift.name} 
-    elevationGain ={lift.elevationGain}
-     status = {lift.status}
-     />)}
-   </div>
-  );
-}
+   return(
+  <div className="App">
+  
+    <StudentList
+     data = {studentData}
+     renderEmpty= {<p>this page is empty... </p>}
+     renderItem = {(item=>(
+      <>{item.name} - {item.elevation} .ft</>
+   ))}
 
+     />
+
+  </div>
+)
+
+}
 
 export default App;
