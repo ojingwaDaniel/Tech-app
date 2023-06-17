a0f25522b74849764eba5333df3e0fbfe4cdad00
ojingwaDaniel <ojingwadanny@gmail.com>
2023-06-17 14:26:26 +0100

    changed the name of the function of the test package

--- src/text.test.js
+++ src/text.test.js
@@ -2,6 +2,6 @@ import { render,screen} from "@testing-library/react";
 import {TextCheck } from "./text";
 test('Checking for a text',()=>{
     render(<TextCheck/>)
-    const text = screen.getByText(/Self Disciplin/);
+    const text = screen.getByText(/Self Discipline/);
     expect(text).toHaveTextContent("Self Discipline");
 })
